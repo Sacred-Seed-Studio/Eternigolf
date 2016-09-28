5b17480c4fb05edcf217b22d6abf919ca7bab5cf
Sarah MacDonald <Sarah MacDonald>
2016-09-28 19:50:04 -0300

    Editor fix.

--- Development/UnityLoader.js
+++ Development/UnityLoader.js
@@ -3499,8 +3499,8 @@ Module.expectedDataFileDownloads++;
                               Module['locateFile'](REMOTE_PACKAGE_BASE) :
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
   
-      var REMOTE_PACKAGE_SIZE = 18201630;
-      var PACKAGE_UUID = 'ffddd6c1-5ae1-4bdc-acad-7d0cebd7405e';
+      var REMOTE_PACKAGE_SIZE = 18201568;
+      var PACKAGE_UUID = 'fbc6c324-8341-48da-ba51-7a43f9203fb3';
     
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
@@ -3604,13 +3604,13 @@ Module['FS_createPath']('/Managed/mono', '2.0', true, true);
       },
     };
 
-      new DataRequest(0, 15347208, 0, 0).open('GET', '/data.unity3d');
-    new DataRequest(15347208, 15348908, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
-    new DataRequest(15348908, 15348929, 0, 0).open('GET', '/PlayerConnectionConfigFile');
-    new DataRequest(15348929, 15353089, 0, 0).open('GET', '/preserved_derived_types.xml');
-    new DataRequest(15353089, 17299033, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
-    new DataRequest(17299033, 18174005, 0, 0).open('GET', '/Resources/unity_default_resources');
-    new DataRequest(18174005, 18201630, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
+      new DataRequest(0, 15347146, 0, 0).open('GET', '/data.unity3d');
+    new DataRequest(15347146, 15348846, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
+    new DataRequest(15348846, 15348867, 0, 0).open('GET', '/PlayerConnectionConfigFile');
+    new DataRequest(15348867, 15353027, 0, 0).open('GET', '/preserved_derived_types.xml');
+    new DataRequest(15353027, 17298971, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
+    new DataRequest(17298971, 18173943, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(18173943, 18201568, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
 
     function processPackageData(arrayBuffer) {
       Module.finishedDataFileDownloads++;
