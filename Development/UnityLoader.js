e3bb334f01350b0c31edcf611409fbc540a93271
Sarah MacDonald <Sarah MacDonald>
2016-09-29 17:46:57 -0300

    Fixed the reset bug.

--- Development/UnityLoader.js
+++ Development/UnityLoader.js
@@ -3499,8 +3499,8 @@ Module.expectedDataFileDownloads++;
                               Module['locateFile'](REMOTE_PACKAGE_BASE) :
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
   
-      var REMOTE_PACKAGE_SIZE = 18276134;
-      var PACKAGE_UUID = 'ebcd3137-6fa6-4248-9700-863305fb7c24';
+      var REMOTE_PACKAGE_SIZE = 18276154;
+      var PACKAGE_UUID = '6c04b396-14b1-4bd9-9fdb-e7da9aa60d18';
     
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
@@ -3608,9 +3608,9 @@ Module['FS_createPath']('/Managed/mono', '2.0', true, true);
     new DataRequest(15416951, 15418716, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
     new DataRequest(15418716, 15418737, 0, 0).open('GET', '/PlayerConnectionConfigFile');
     new DataRequest(15418737, 15422897, 0, 0).open('GET', '/preserved_derived_types.xml');
-    new DataRequest(15422897, 17373537, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
-    new DataRequest(17373537, 18248509, 0, 0).open('GET', '/Resources/unity_default_resources');
-    new DataRequest(18248509, 18276134, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
+    new DataRequest(15422897, 17373557, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
+    new DataRequest(17373557, 18248529, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(18248529, 18276154, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
 
     function processPackageData(arrayBuffer) {
       Module.finishedDataFileDownloads++;
